eeca423c84a35c31eef688ead549567524317c61
Yrmee <80338690+Yrmee@users.noreply.github.com>
2021-12-09 12:29:53 +0100

    Update App.js
    Opmerking toegevoegd voor Docent.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+/* 
+Ik heb deze opdracht heel slordig gemaakt, ik vond de opdracht heel moeilijk te lezen.
+Ik wist ook niet dat deze opdracht nagekeken zou worden met een reactie van de docent,
+dus hier en daar zullen enorm veel schoonheidsfouten inzitten. Maar ik was zo klaar met deze 
+opdracht dat ik hem ingeleverd heb. Excuses alvast.
+*/
+
+
 import React from 'react';
 import './App.css';
 import Container from './Container';
